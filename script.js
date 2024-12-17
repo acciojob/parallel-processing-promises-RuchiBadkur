1cc36968ee1d0e3c05566f4e268547639128089f
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-12-17 15:18:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,63 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const getImages = (images) => {
+	const [img1, img2, img3] = images;
+	const myPromise1 = new Promise((res, rej) => {
+		res(img1.url)
+	})
+
+	const myPromise2 = new Promise((res, rej) => {
+		res(img2.url)
+	})
+
+	const myPromise3 = new Promise((res, rej) => {
+		res(img3.url)
+	})
+
+	Promise.all([myPromise1, myPromise2, myPromise3])
+	.then((list) => {
+		console.log(list);
+		list.map((item) => {
+			console.log(item);
+			output.innerHTML = `
+				<img href=${item} width="400px" height="200px"/>
+			`
+		})
+	})
+	.catch((err) => {alert(err)})
+	
+}
+
+btn.addEventListener("click", ()=>getImages(images));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
